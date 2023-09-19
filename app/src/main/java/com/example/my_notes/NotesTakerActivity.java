ca224d59c02e8029114a04bd77166706126bf5a1
AnastasiyaK98 <yanastyana@mail.ru>
2023-09-20 03:38:38 +0500

    Добавление текста в strings Обработка события при нажатии кнопки сохранить при создании заметки

--- app/src/main/java/com/example/my_notes/NotesTakerActivity.java
+++ app/src/main/java/com/example/my_notes/NotesTakerActivity.java
@@ -2,12 +2,20 @@ package com.example.my_notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.my_notes.Models.PersonalNotes;
+import com.example.my_notes.Models.WorkNotes;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NotesTakerActivity extends AppCompatActivity {
 
@@ -23,6 +31,11 @@ public class NotesTakerActivity extends AppCompatActivity {
     String folder_name;
     //Полученное предыдущее название раздела
     String getSelected_folder;
+
+    //Объект заметки "Личное"
+    PersonalNotes personalnotes;
+    //Объект заметки "Работа"
+    WorkNotes worknotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +72,66 @@ public class NotesTakerActivity extends AppCompatActivity {
                 startActivity(intent);
                 //Завершение текущего Activity
                 finish();
+            }
+        });
+
+        //Действия при нажатии на кнопку сохранить
+        imageView_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Получение названия заметки
+                String title=editText_title.getText().toString();
+                //Получение текста заветки
+                String description=editText_notes.getText().toString();
+                //Проверка введён ли текст заметки
+                if (description.isEmpty()) {
+                    //Всплывающее сообщение
+                    Toast.makeText(NotesTakerActivity.this, R.string.no_notes_text, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Формат даты - день недели, число, месяц, год, время- часы:минуты
+                SimpleDateFormat formater= new SimpleDateFormat("EEE, d MMM yyyy HH:mm");
+                Date date = new Date();
+
+                //Если заметка создаётся в разделе "Личное"
+                if (getSelected_folder.equalsIgnoreCase("Личное")) {
+                    //Создание новой заметки
+                    personalnotes = new PersonalNotes();
+                    //Получение названия заметки из поля ввода
+                    personalnotes.setTitle(title);
+                    //Получение текста заметки из поля ввода
+                    personalnotes.setText(description);
+                    //Получение даты создания заметки
+                    personalnotes.setDate(formater.format(date));
+                    //Создание новой Activity
+                    Intent intent = new Intent();
+                    //Передача заметки
+                    intent.putExtra("note", personalnotes);
+                    //Возвращение результата в вызвавшую активность
+                    setResult(Activity.RESULT_OK, intent);
+                    //Завершение активности
+                    finish();
+                }
+                //Если заметка создаётся в разделе "Работа"
+                else if (getSelected_folder.equalsIgnoreCase("Работа")) {
+                    //Создание новой заметки
+                    worknotes = new WorkNotes();
+                    //Получение названия заметки из поля ввода
+                    worknotes.setTitle(title);
+                    //Получение текста заметки из поля ввода
+                    worknotes.setText(description);
+                    //Получение даты создания заметки
+                    worknotes.setDate(formater.format(date));
+                    //Создание новой Activity
+                    Intent intent = new Intent();
+                    //Передача заметки
+                    intent.putExtra("note",   worknotes);
+                    //Возвращение результата в вызвавшую активность
+                    setResult(Activity.RESULT_OK, intent);
+                    //Завершение активности
+                    finish();
+                }
             }
         });
 
