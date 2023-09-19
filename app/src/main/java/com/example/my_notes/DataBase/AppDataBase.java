26b053c9d1f139cd5f4515dedfa362627082c7d7
AnastasiyaK98 <yanastyana@mail.ru>
2023-09-19 21:18:33 +0500

    Создание класса базы данных

---
+++ app/src/main/java/com/example/my_notes/DataBase/AppDataBase.java
@@ -0,0 +1,32 @@
+package com.example.my_notes.DataBase;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.my_notes.Models.PersonalNotes;
+import com.example.my_notes.Models.WorkNotes;
+
+//@Database - аннотация для объявления базы данных
+@Database(entities = {PersonalNotes.class, WorkNotes.class}, version = 1, exportSchema = false)
+public abstract class AppDataBase extends RoomDatabase {
+    private static AppDataBase database;
+    private static String DataBase_Name = "AppMyNotes";
+    //Создание и возврат Базы Данных (доступно из других ативностей)
+    //Если база данных не создана - то она создаётся
+    //allowMainThreadQueries() - разрешает запросы к бд в главном потоке
+    //fallbackToDestructiveMigration() - разрешено деструктивно воссоздавать таблицы базы данных из определенных версий
+    public synchronized static AppDataBase getInstance(Context context) {
+        if (database == null) {
+            database = Room.databaseBuilder(context.getApplicationContext(), AppDataBase.class, DataBase_Name)
+                    .allowMainThreadQueries().fallbackToDestructiveMigration()
+                    .build();
+        }
+        return database;
+    }
+    //Доступ к манипулированию данными
+    public abstract PersonalDAO noteDAO();
+    public abstract WorkDAO workDAO();
+}
